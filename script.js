1a289793ece9fd761e41ccb1b29518a5933bcafd
koko <github@koko-campus.com>
2022-09-09 21:57:13 +0900

    確率を算出する関数まで。

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 "use strict";
 
+const [Nbox, probabilityLine] = getElm(["Nbox", "probabilityLine"]);
+
+const solver = (n, k) => 1 - (power(Math.exp(1), -1 * k * (k - 1) / (2 * n)));
+
+Nbox.addEventListener("input", function() {
+    const n = parseInt(this.value);
+    const points = [];
+    doNtimes(n, k => {
+        const p = solver(n, k);
+
+    });
+});
+
 
